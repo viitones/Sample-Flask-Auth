043ef842465035f2cf9e1ee3920efd646d8aa629
viitones <vitonesmartins@gmail.com>
2025-03-19 00:54:52 -0300

    adiciona configuração do banco de dados e modelo de usuário

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from db import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
-
+db.init_app(app) 
 
 if __name__ == '__main__':
     app.run(debug=True)

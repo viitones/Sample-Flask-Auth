78c444f1be8e39990c6d1be166654fd1ee77d91e
viitones <vitonesmartins@gmail.com>
2025-03-23 00:26:02 -0300

    implementa hash de senha usando bcrypt na criação e autenticação de usuário

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import bcrypt
 from flask import Flask, request, jsonify
 from models.user import User
 from db import db
@@ -26,7 +27,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if username and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.username)
             return jsonify({"message": "Autenticado com sucesso"}), 200
@@ -46,7 +47,9 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password, role="user")
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+
+        user = User(username=username, password=hashed_password, role="user")
         db.session.add(user)
         db.session.commit()
 
